bc3564d821fb6af4d10bb17d4fc31e6dfc959b72
sharvey <bundito@users.noreply.github.com>
2018-06-06 10:58:38 -0500

    Initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,17 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+admin:21232f297a57a5a743894a0e4a801fc3:Admin:admin,user
+bundito:9f6630d03b18ec9f2c6342b00964433e:Dito:bundito@gmail.com:admin,user
+kakers:$1$Ea9VZwQL$S4q3VsfAy9n7HCYrI/f/a1:Kristen:bundito@gmail.com:user
+dave:$1$3JdkVqBj$M.VQHfwy1GZ.CS2DKDyHO/:Dave McCann:discoboy03@comcast.net:user
+paul:$1$CgidjhEj$7Xe81OwStVBlsSvbQgCTz/:Paul Jacobson:Paul.J@avatarproductions.com:user
+steve:$1$4bSm2eWV$YXZje2DRcyq6bziwgGbU00:Steve Juras:steve.juras@gmail.com:user
+stevegoz:$1$aNivhGcr$a4LHh7Y/8WQGWnyT96kvh.:Steve Gozdecki:stevegoz@gmail.com:user
